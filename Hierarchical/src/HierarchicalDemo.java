3784da17c64558bea73dc749a1cfe51c9e2f5a1e
Thenmozhi-Cheliyan <98871555+Thenmozhi-Cheliyan@users.noreply.github.com>
2022-02-27 22:31:08 +0530

    Second Commit

--- Hierarchical/src/HierarchicalDemo.java
+++ Hierarchical/src/HierarchicalDemo.java
@@ -22,3 +22,21 @@ public class HierarchicalDemo
 	}
 
 }
+
+/*	Output
+ Enter Salary:
+40000
+*********Employee*********
+Initial Salary = 40000.0
+HRA = 800.0
+PF = 1200.0
+*********Permanent Employee*********
+The incremented Salary of permanent Employee is 60000.0
+New HRA = 1200.0
+New PF = 1800.0
+*********Temproroay Employee*******
+The incremented Salary of Temprorary Employee is 54000.0
+New HRA = 1080.0
+New PF = 1620.0
+
+*/
